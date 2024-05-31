cc1d958fa70dfba79dd652e3cba514eeacf28a5b
andrey <yagunov02@bk.ru>
2024-05-31 19:43:01 +0500

    перенос фала

--- src/view/location-view.js
+++ src/view/location-view.js
@@ -17,7 +17,7 @@ export class LocationView extends BaseView {
 
         this.drawLinesFromLocationToElements(svg, centerX, centerY, radius, this.elements);
         this.drawLocationRectangle(svg, centerX, centerY, this.location);
-        this.drawElementsInCircle(svg, centerX, centerY, radius, this.elements, (element) => this.presenter.onElementClick(element));
+        this.drawElementsInCircle(svg, centerX, centerY, radius, this.elements, (element) => this.presenter.onLocationClick(element));
     }
 
     drawLinesFromLocationToElements(svg, centerX, centerY, radius, elements) {
